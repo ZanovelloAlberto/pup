33bd85e51bc62972e9ccbffe43b60d8aa63c4f1c
Alberto Zanovello <zanovello2002@gmail.com>
2021-09-20 00:37:34 +0200

    facendo

--- index.ts
+++ index.ts
@@ -1,28 +1,56 @@
 import puppeteer from "puppeteer"
 
-
-
-
-
-(async () => {
+const run = async (link: string, pageCall: () => void) => {
     const browser = await puppeteer.launch({
-        headless: false,
+        headless: true,
         slowMo: 100, // slow down by 250ms
     });
-    try {
-        const [page] = await browser.pages();
 
-        await page.goto('https://altadefinizione.sale/?s=matrix');
+    const [page] = await browser.pages();
 
-        const data = await page.evaluate(() => {
-            const divs = document.querySelectorAll('.col-lg-3.col-md-4.col-xs-4.mb-30');
-            console.log(divs.item(1));
-             
-            const imgSources = Array.from(divs, div => div.querySelector("a")?.href);
-            return imgSources;
-        });
-        console.log(data);
+    await page.goto(link);
+
+    const data  = await page.evaluate(pageCall);
+
+    console.log(data);
+
+
+
+}
+const clvid = "plyr__video-wrapper"
+
+const getLinks = () => {
+
+    const search = "mad max"
+
+    run('https://altadefinizione.sale/?s=' + search, () => {
+
+        console.log("imgSources");
+        const divs = document.querySelectorAll('.col-lg-3.col-md-4.col-xs-4.mb-30')
+        const imgSources = Array.from(divs, div => div.querySelector("a")?.href)
+
+    })
+
+}
+
+
+
+const getMp4 = () => {
+
+    run('https://altadefinizione.sale/mad-max-interceptor-streaming-4k/')
+    // ()=>{
+        const a = document.getElementById("iframeVid")
+        const divs = document.querySelectorAll("Oppure, continua come ospite e guarda le pubblicità")
+        console.log("a");
         
-        
-    } catch (err) { console.error(err); } finally { await browser.close(); }
-})();
+    }
+
+
+}
+
+getMp4()
+
+
+
+
+
